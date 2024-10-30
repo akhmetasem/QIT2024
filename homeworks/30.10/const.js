281402657560fae0292072055372ca5d0533a41b
Asem <naziranurislam2014>
2024-10-30 12:49:31 +0500

    tapsirma

--- homeworks/30.10/const.js
+++
@@ -1,8 +0,0 @@
-var name = "Aty";//өзгеретін мән
-let surname = "Familia";//өзгеретін мән
-surname ="ABC";//айнымалының мәні өзгереді
-const id = 929292;//number
-id = 2584544;//qate qaitalandi
-console.log(id);//Sjiqpaidy
-
-

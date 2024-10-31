c4655dfeff55e56d3185edb0c28d636ca5b9eba7
Asem <naziranurislam2014>
2024-10-31 12:13:30 +0500

    tapsirma

---
+++ examples/31.10/index.js
@@ -0,0 +1,57 @@
+//let x = 4;
+ //let y = 10;
+ //let result = x + y;// операция
+ //console.log(result)
+ 
+
+// var a = 30;
+ //var b = 20;
+// console.log(a-b);
+
+ //var x = 45;
+// var y = 5;
+ //var result = x*y;//225
+ //console.log(result)
+
+ //var c = 20;
+// var d = 2;
+ //console.log(20/2); // 10
+
+ //var a  = 20; // tagaindau
+ // ten ekenin tekseru ushin
+ // (a % 2 = 0) { // = =
+   // console.log("a, sany jup san")
+ //}
+
+ ///var num = 100;
+// num++;//increment
+ //num--;//decrement
+ //console.log(num);
+
+ //var a = 100;
+ //a += 5;// qosip tagaindau a = a + 5;
+// console.log(a);
+
+ //var a = 100;
+ //a -= 5;//a = a - 5;
+ //a/= 5;//a = a / 5;
+ //a*= 5; // a = a *5;
+// a %= 3; // a = a % 3; a = 1;
+// console.log(a);
+
+// var a = 20;
+// var b = 30;
+// var c = 5;
+// var d = 2;
+ //var c = 10;
+ //var e = e;
+ //let total = ((a + b) * c - d / e);
+ //console.log(total);
+
+let a =  45;
+let b = 47;
+let c = 59;
+let z = 5;
+let x = 3;
+let total = ((a*b) / c) / (z - 3);
+console.log(total % 2)
